35fea6aba9cf380258aa5451b642481f0e647f49
nchamurlijska <67510952+nchamurlijska@users.noreply.github.com>
2020-08-21 15:49:52 +0300

    Add files via upload

---
+++ CountWordsMethod.java
@@ -0,0 +1,15 @@
+
+public class CountWordsMethod {
+
+	public static int countWords(String wordOne,String wordTwo, String wordThree,String wordFour) {
+		int count = countWords
+		return count;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Tree.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tree {
+
+	public static void main(String[] args) {
+		String p = "*";
+		System.out.println("      *");
+		System.out.println("     ***");
+		System.out.println("    *****");
+		System.out.println("   *******");
+		System.out.println("  *********");
+		System.out.println(" ***********");
+		System.out.println("*************");
+		System.out.println("    *****");
+		System.out.println("    *****");
+		
+}
+}
